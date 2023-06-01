5669cfc4b0e66209502207bc46089489002a6cbb
RedGhost1505 <100959515+RedGhost1505@users.noreply.github.com>
2023-06-01 10:25:50 -0600

    Cors integration

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template, jsonify, abort, request
+from flask_cors import CORS, cross_origin
 
 uri='/api/productos/'
 app= Flask(__name__)
+cors = CORS(app)
+
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+@app.route("/")
+
+@cross_origin()
 
 
 
